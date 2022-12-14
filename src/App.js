c3de362ccc76ae87d2c6c19e137a1cb1a3d8161e
auziqni <auziqni@gmail.com>
2022-12-15 02:09:54 +0700

    component and props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+function Biodata(props) {
+    return <span>umur {props.umur}</span>;
+}
+function Panggilnama(props) {
+    return (
+        <h1>
+            hallo juga {props.nama}, sebagai seorang {props.profesi},{" "}
+            <Biodata umur={props.umur} />
+        </h1>
+    );
+}
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <h1> hello world</h1>
+                <Panggilnama nama="auziqni" profesi="programmer" umur="23" />
+                <Panggilnama nama="ziqni" profesi="guru" umur="23" />
             </header>
         </div>
     );
